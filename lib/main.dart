848afdeacb08a4ccfde25e0f244d49887b00b035
Desarrollador 04 <victorc@smartp.co>
2024-12-04 14:35:27 -0500

    terminando el primer modulo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:holamundo/screens/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,40 +20,21 @@ class RecipeBook extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.orange,
-        title: Text(
-          "Recipe Book",
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: 125,
-        child: Card(
-          child: Row(children: <Widget>[
-            Container(
-              height: 125,
-              width: 100,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Container(),
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.orange,
+              title: Text(
+                "Recipe Book",
+                style: TextStyle(color: Colors.white),
               ),
+              bottom: TabBar(
+                  indicatorColor: Colors.white,
+                  labelColor: Colors.white,
+                  unselectedLabelColor: Colors.white,
+                  tabs: [Tab(icon: Icon(Icons.home), text: "Home")]),
             ),
-            SizedBox(
-              width: 26,
-            ),
-            Column(
-              children: <Widget>[
-                Text("Aureliano Buendia"),
-                Text("Ensalada dulce"),
-                Container(height: 2, width: 75, color: Colors.orange)
-              ],
-            ),
-          ]),
-        ),
-      ),
-    );
+            body: TabBarView(children: [HomeScreen()])));
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,55 +3,20 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: 125,
-        child: Card(
-          child: Row(children: <Widget>[
-            Container(
-              height: 125,
-              width: 100,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Container(),
-              ),
-            ),
-            SizedBox(
-              width: 26,
-            ),
-            Column(
-              children: <Widget>[
-                Text("Aureliano Buendia"),
-                Text("Ensalada dulce"),
-                Container(height: 2, width: 75, color: Colors.orange)
-              ],
-            ),
-          ]),
-        ),
-      ),
-    );
-    ;
+        body: Column(children: <Widget>[
+      _RecipesCard(context),
+      _RecipesCard(context),
+      _RecipesCard(context)
+    ]));
   }
-}
 
-class RecipeBook extends StatelessWidget {
-  const RecipeBook({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.orange,
-        title: Text(
-          "Recipe Book",
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: Container(
+  Widget _RecipesCard(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
         width: MediaQuery.of(context).size.width,
         height: 125,
         child: Card(
@@ -59,19 +24,26 @@ class RecipeBook extends StatelessWidget {
             Container(
               height: 125,
               width: 100,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Container(),
-              ),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),color:Colors.orange),
             ),
             SizedBox(
               width: 26,
             ),
             Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text("Aureliano Buendia"),
-                Text("Ensalada dulce"),
-                Container(height: 2, width: 75, color: Colors.orange)
+                Text("Aureliano Buendia",
+                    style: TextStyle(fontSize: 16, fontFamily: "Quicksand")),
+                SizedBox(
+                  height: 4,
+                ),
+                Container(height: 2, width: 75, color: Colors.orange),
+                SizedBox(
+                  height: 4,
+                ),
+                Text("Ensalada dulce",
+                    style: TextStyle(fontSize: 16, fontFamily: "Quicksand")),
               ],
             ),
           ]),
